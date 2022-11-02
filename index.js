605eeb69716178439093ba51604d7c824cc25b6f
virtual-devs <virtual.devs2022@gmail.com>
2022-11-02 03:02:00 -0600

    change

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+import  express from 'express';
+
 const app = express();
-var cors = require('cors');
+
 
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.json());
-app.use(cors());
 
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
