e437f3442a86b8f0e3ac0ec83392996a938f9bdc
virtual-devs <virtual.devs2022@gmail.com>
2022-11-13 12:31:38 -0600

    config .env

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import app from "./app.js";
 import { sequelize } from "./database/database.js";
 // import './models/HistorialRenta.js'
 // import './models/HistorialViaje.js'
+import {PORT} from './config.js'
 
 async function main() {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-    app.listen(3000);
-    console.log("Server on port", 3000);
+    app.listen(PORT);
+    console.log("Server on port", PORT);
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
