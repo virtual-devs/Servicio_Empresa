af2e9f070cc86c39294e4dbdafcd299466a40866
virtual-devs <virtual.devs2022@gmail.com>
2022-11-23 23:51:57 -0600

    filtro fecha

--- src/routes/viaje.routes.js
+++ src/routes/viaje.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {getViajes, getViaje, createViaje, updateViaje, deleteViaje, getViajeOne} from "../controllers/viaje.controller.js";
+import {getViajes, getViaje, createViaje, updateViaje, deleteViaje, getViajeOne, getFiltroViajeFecha} from "../controllers/viaje.controller.js";
 
 const router = Router();
 
 router.get("/viajes", getViajes);
 router.get("/viajes/:id", getViaje);
 router.get("/viajeOne/:id", getViajeOne);
+router.get("/viajesFecha/:fecha", getFiltroViajeFecha)
 router.post("/viajesAdd", createViaje);
 router.put("/viajesUpdate/:id", updateViaje);
 router.delete("/viajesRemove/:id", deleteViaje);
